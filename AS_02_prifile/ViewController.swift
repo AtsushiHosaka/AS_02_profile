a36aef27699a5c5102e7e7536eb2b9a965ba2287
AtsushiHosaka <hosaka2917@gmail.com>
2022-05-11 16:15:55 +0900

    Initial Commit

---
+++ AS_02_prifile/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AS_02_prifile
+//
+//  Created by 保坂篤志 on 2022/05/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
